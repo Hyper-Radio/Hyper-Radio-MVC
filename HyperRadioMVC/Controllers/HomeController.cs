9a276cfe6371a7d4eafee872606e536e7591f967
joshuauaua <joshjoelng@gmail.com>
2025-10-07 09:51:25 +0200

    Initial commit from Rider

---
+++ HyperRadioMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HyperRadioMVC.Models;
+
+namespace HyperRadioMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var artist = new ArtistProfile
+        {
+            Name = "Frank Ocean",
+            Description = "Frank Ocean is an American singer, songwriter, record producer, and photographer. Known for his unconventional production and eclectic musical style, he has been acclaimed as one of the most influential contemporary artists.",
+            ImageUrl = "https://www.nme.com/wp-content/uploads/2016/09/2011FrankOcean02PR070212.jpg"
+        };
+
+        return View(artist);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ HyperRadioMVC/ViewModels/ArtistProfileVM.cs
@@ -0,0 +1,10 @@
+namespace HyperRadioMVC.ViewModels;
+
+public class ArtistProfileVM
+{
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+}
