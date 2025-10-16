a1b5364faad8d1dce0c56bb628b06e9b69b32e54
joshuauaua <joshjoelng@gmail.com>
2025-10-16 10:30:03 +0200

    Updated multistep pitch form

--- HyperRadioMVC/Models/Track.cs
+++ HyperRadioMVC/Models/Track.cs
@@ -6,7 +6,14 @@ public class Track
     public string Type { get; set; }
     public string ImageUrl { get; set; }
     public string TrackUrl { get; set; }
+    public string Genre { get; set; }
     public string Description { get; set; }
     public string Lyrics { get; set; }
-    public int Duration { get; set; }
+    public int ReleaseYear { get; set; }
+    public string Neighborhood { get; set; }
+    public string TrackFile { get; set; }
+    public string RecordingStory { get; set; }
+    public string Story { get; set; }
+    
+    public string VoiceMemo { get; set; }
 }
