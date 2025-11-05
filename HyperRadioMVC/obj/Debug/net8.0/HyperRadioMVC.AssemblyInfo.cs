b719925d5614edc981d54b2ac0cf612f991be107
joshuauaua <joshjoelng@gmail.com>
2025-11-05 12:40:04 +0100

    More styling commits, especially in track pitch page

--- HyperRadioMVC/obj/Debug/net8.0/HyperRadioMVC.AssemblyInfo.cs
+++ HyperRadioMVC/obj/Debug/net8.0/HyperRadioMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HyperRadioMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22979e5d7d0052e1a85858c241e584bb6e72b785")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3dd04deadf42598407e8eb4cb062cc522a3a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("HyperRadioMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HyperRadioMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
