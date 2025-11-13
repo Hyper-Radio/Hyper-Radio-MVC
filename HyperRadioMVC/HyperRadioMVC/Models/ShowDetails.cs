6b5b09aa6460b1e44e6f2ffd4aee83d6a43ed2f6
joshuauaua <joshjoelng@gmail.com>
2025-11-14 07:00:24 +0800

    For each show in show list is now clickable, returns Id that on click will load the show (int:id) into streamer, destroying currently playing stream and get new stream live link for player

---
+++ HyperRadioMVC/HyperRadioMVC/Models/ShowDetails.cs
@@ -0,0 +1,14 @@
+namespace HyperRadioMVC.Models;
+
+public class ShowDetails
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime ScheduledStart { get; set; }
+    public string URL { get; set; }
+
+    public string ImageUrl { get; set; }
+    
+}
