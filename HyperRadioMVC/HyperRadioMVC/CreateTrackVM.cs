2af25ec29c4fc00e8f59914ec8e2141f55012043
Joshua Ng <78846792+joshuauaua@users.noreply.github.com>
2025-11-19 14:03:50 +0700

    Add files via upload

---
+++ HyperRadioMVC/HyperRadioMVC/CreateTrackVM.cs
@@ -0,0 +1,15 @@
+namespace HyperRadioMVC.ViewModels;
+
+public class CreateTrackVM
+{
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+    public string Genre { get; set; }
+    public string Description { get; set; }
+
+    public int Duration { get; set; }
+    
+    public IFormFile File  { get; set; }
+    public string TrackURL { get; set; }
+    public string? ImageURL { get; set; }
+}
