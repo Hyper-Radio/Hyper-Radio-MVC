85d6689fcdbe690ee49da97570e57b7372691f5f
Joshua Ng <78846792+joshuauaua@users.noreply.github.com>
2025-11-20 11:28:39 +0700

    To get around CORS issue, created a proxy in controllers and then able to perform CRUD operations from MVC to API
    Have applied for both forms (Create Show and Post track)

--- HyperRadioMVC/HyperRadioMVC/Controllers/PitchController.cs
+++ HyperRadioMVC/HyperRadioMVC/Controllers/PitchController.cs
@@ -1,26 +1,66 @@
 using HyperRadioMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-namespace HyperRadioMVC.Controllers;
-
-public class PitchController : Controller
+namespace HyperRadioMVC.Controllers
 {
-    
-    private readonly HttpClient _client;
-
-    public PitchController(IHttpClientFactory clientFactory)
+    public class PitchController : Controller
     {
-        _client = clientFactory.CreateClient("Hyper-Radio-API");
-    }
-    
-    //CREATE FORM
-    [HttpGet]
-    public IActionResult Index()
-    {
-        return View();
-    }
-    
+        private readonly IHttpClientFactory _clientFactory;
 
-    
-    
+        public PitchController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateTrackVM model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var client = _clientFactory.CreateClient("Hyper-Radio.API");
+
+            using var content = new MultipartFormDataContent();
+            content.Add(new StringContent(model.Title), "Title");
+            content.Add(new StringContent(model.Description), "Description");
+            content.Add(new StringContent(model.Genre), "Genre");
+            content.Add(new StringContent(model.ReleaseYear.ToString()), "ReleaseYear");
+            content.Add(new StringContent(model.ImageURL ?? ""), "ImageURL");
+
+            if (model.File != null && model.File.Length > 0)
+            {
+                var streamContent = new StreamContent(model.File.OpenReadStream());
+                streamContent.Headers.ContentType = new MediaTypeHeaderValue(model.File.ContentType);
+                content.Add(streamContent, "File", model.File.FileName);
+            }
+
+            try
+            {
+                var response = await client.PostAsync("/api/tracks", content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    ModelState.AddModelError("", $"API error: {response.StatusCode}");
+                    return View(model);
+                }
+
+                TempData["Success"] = "Track submitted successfully!";
+                return RedirectToAction("Index");
+            }
+            catch (HttpRequestException ex)
+            {
+                ModelState.AddModelError("", $"Error submitting track: {ex.Message}");
+                return View(model);
+            }
+        }
+    }
 }
---
+++ HyperRadioMVC/HyperRadioMVC/Controllers/TrackController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HyperRadioMVC.Controllers
+{
+    [Route("api/[controller]")]
+    public class TrackController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TrackController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTracks()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var apiUrl = "https://hyper-radio-api-ezgpemf9d5g2cuc0.norwayeast-01.azurewebsites.net/api/tracks";
+
+            try
+            {
+                var response = await client.GetAsync(apiUrl);
+                var content = await response.Content.ReadAsStringAsync();
+                return Content(content, "application/json");
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, $"Error fetching tracks: {ex.Message}");
+            }
+        }
+    }
+}
